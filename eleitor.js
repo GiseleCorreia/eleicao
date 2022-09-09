c51c5283feeb664d3db002031208a518bf8d419b
GiseleCorreia <mariagisele93@hotmail.com>
2022-09-08 21:07:39 -0300

    eleicao

---
+++ eleitor.js
@@ -0,0 +1,14 @@
+function consulta() {
+  var idade = document.getElementById('idade').value
+
+  var elementoIdade = document.getElementById('consulta')
+  elementoIdade
+
+  if (idade < 16) {
+    elementoIdade.innerHTML = 'Não eleitor!'
+  } else if (idade >= 18 && idade <= 70) {
+    elementoIdade.innerHTML = 'Eleitor obrigatório!'
+  } else if (16 <= idade < 18 || idade < 70) {
+    elementoIdade.innerHTML = 'Eleitor facultativo!'
+  }
+}
